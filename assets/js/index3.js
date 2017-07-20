ddfa30786c89098245428b9ea7297170505d2a4d
chiragshahklc <chiragshahklc@gmail.com>
2017-07-20 10:55:58 +0530

    added js to index3.html
    click event for spans which shows 4 options

---
+++ assets/js/index3.js
@@ -0,0 +1,38 @@
+var spans = document.getElementsByClassName("input-group-addon");
+// spans.addEventListener("click",spanClicked);
+for (var i = 0; i < spans.length; i++) {
+  spans[i].addEventListener("click", spanClicked);
+}
+var counter = 0;
+
+function spanClicked() {
+  if (counter < 4) {
+    var x = document.getElementById(this.id);
+    counter++;
+    x.innerHTML = counter;
+    x.style.backgroundColor = "grey";
+  }
+}
+
+// array.forEach(function(element) {
+
+// }, this);
+
+// $(".ct").click(function() {
+//     debugger
+// var SpanId='';
+//   if (counter < 4 ) {
+//     counter++;
+
+//     var ClickText = $(this).text();
+//     SpanId=this.id;
+//     alert(SpanId);
+//     alert($(SpanId).text());
+//     $(SpanId).text();
+//     alert(counter);
+//   }
+// });
+
+// loop nu banavelu che ?
+
+// taro avaj km nthi aavto?

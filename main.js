7553069511808a3d9564b8cc37aafe6f9b2e380d
chiragshahklc <chiragshahklc@gmail.com>
2017-07-22 00:32:04 +0530

    heavy updates

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 var express = require("express");
 var path = require("path");
 var app = express();
+var bodyParser = require("body-parser");
+
+var urlEncodedparser = bodyParser.urlencoded({ extended: false });
+
+app.set("view engine", "ejs");
 
 app.use("/assets", express.static(__dirname + "/assets"));
 
-
-
-app.get("/", function(req, resp){
-    resp.sendFile("index.html", {root: path.join(__dirname, "/files")});
-})
+app.get("/", function(req, resp) {
+//   resp.sendFile("index.html", { root: path.join(__dirname, "/files") });
+    console.log("This is ejs request");
+    resp.render("index");
+});
 
 app.get(/^(.+)$/, function(req, resp) {
-    console.log(req.params[0]);
-    resp.sendFile(req.params[0] + ".html", {root: path.join(__dirname + "/files")});
-})
+  console.log(req.params[0]);
+  resp.sendFile(req.params[0] + ".html", {
+    root: path.join(__dirname + "/files")
+  });
+});
 
-app.post("/index3", function(req, resp){
-    console.log("this is post method");
-    resp.redirect("/index3");
-})
+app.post("/trial", urlEncodedparser, function(req, resp) {
+  console.log(req.body);
+  resp.render("trial", { data: req.body });
+});
 
-app.listen(80, function(){
-    console.log("Listening on Port 80.");
-})
+app.listen(80, function() {
+  console.log("Listening on Port 80.");
+});

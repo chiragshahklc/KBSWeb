30d2ad86b57de98a6c4590501a402d155da95c24
Kirtirajsinh Solanki <kirtiraj2310@gmail.com>
2017-07-22 14:23:39 +0530

    Roles are assigned

--- main.js
+++ main.js
@@ -2,23 +2,39 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var bodyParser = require("body-parser");
+var sessions = require("express-session");
 
 var urlEncodedparser = bodyParser.urlencoded({ extended: false });
 
 app.set("view engine", "ejs");
 app.use(urlEncodedparser);
 app.use("/assets", express.static(__dirname + "/assets"));
+app.use(sessions({ secret: "xxx" }));
+
+var sess;
 
 app.get("/", function(req, resp) {
   //   resp.sendFile("index.html", { root: path.join(__dirname, "/files") });
-  console.log("This is ejs request");
-  resp.render("index");
+
+  sess = req.session;
+
+  if (sess.name) {
+    if (sess.name == "shadev2012") {
+      resp.render("admin");
+    } else if (sess.name == "public") {
+      resp.render("public");
+    } else {
+      resp.render("player");
+    }
+  } else {
+    resp.render("index");
+  }
 });
 
 app.get(/^(.+)$/, function(req, resp) {
   // console.log(req.params[0]);
   var tmp = req.params[0];
-  var x =tmp.replace("/","");
+  var x = tmp.replace("/", "");
   // console.log(x);
   // resp.sendFile(req.params[0] + ".html", {
   //   root: path.join(__dirname + "/files")
@@ -26,11 +42,23 @@ app.get(/^(.+)$/, function(req, resp) {
   resp.render(x, { data: req.body });
 });
 
-app.post("/trial", urlEncodedparser, function(req, resp) {
-  console.log(req.body);
-  resp.render("trial", { data: req.body });
+app.post("/login", urlEncodedparser, function(req, resp) {
+  sess = req.session;
+  sess.name = req.body.name;
+  sess.pass = req.body.password;
+  if (sess.name === "shadev2012" && sess.pass === "") {
+    resp.render("admin", { data: req.body });
+  } else if (sess.pass === "") {
+    resp.render("player", { data: req.body });
+  } else if (sess.name === "public") {
+    resp.render("public", { data: req.body });
+  }
+  else
+    {
+      resp.render("index", { data: req.body });
+    }
 });
 
-app.listen(process.env.PORT || 80, function() {
+app.listen(process.env.PORT || 88, function() {
   console.log("Listening on Port 80.");
 });
